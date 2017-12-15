5e83ba899cde2335e01154b6c069cda84a3c3503
nalssee <kenjin@sdf.org>
2017-12-16 03:26:51 +0900

    ..

--- sqlplus/core.py
+++ sqlplus/core.py
@@ -277,15 +277,16 @@ class Rows:
         return self._newrows([r for r in self
                               if all(isinstance(r[c], str) for c in cols)])
 
-    def avg(self, col, wcol=None, n=3):
+    def avg(self, col, wcol=None, n=None):
         # wcol: column for weight,
         # n: you may want to round up
         if wcol:
             rs = self.isnum(col, wcol)
             total = sum(r[wcol] for r in rs)
-            return round(sum(r[col] * r[wcol] for r in rs) / total, n)
+            val = sum(r[col] * r[wcol] for r in rs) / total
         else:
-            return round(st.mean(r[col] for r in self if isnum(r[col])), n)
+            val = st.mean(r[col] for r in self if isnum(r[col]))
+        return round(val, n) if n else val
 
     # Simple one but..
     def ols(self, model):
