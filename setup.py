05f4ba8079a940cbc072397c1e00aa0e79f7d77a
nalssee <kenjin@sdf.org>
2018-02-02 13:16:00 +0900

    ..

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sqlplus',
-      version='1.1.2',
+      version='1.1.3',
       description='data work tools',
       url='https://github.com/nalssee/sqlplus.git',
       author='nalssee',
